44b2e9921287c568c2752db88b5a85a7f8e823bd
josemfiguero04 <josemfigueroa04@gmail.com>
2023-11-11 13:18:33 -0300

    add Image

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import AcmeLogo from '@/app/ui/acme-logo';
 import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import {lusitana} from '@/app/ui/fonts';
+import Image from 'next/image';
 
 import Link from 'next/link';
 
 export default function Page() {
+
   return (
     <main className="flex min-h-screen flex-col p-6">
       <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
@@ -32,6 +34,20 @@ export default function Page() {
         </div>
         <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
           {/* Add Hero Images Here */}
+          <Image
+          src={'/hero-destop.png'}
+          width={1000}
+          height={760}
+          alt='Screenshots of the dashboard project showing desktop and mobile versions'
+          className='hidden md:block'
+          />
+          <Image
+          src={'/hero-mobile.png'}
+          width={560}
+          height={6200}
+          alt='Screenshots of the dashboard project showing desktop and mobile versions'
+          className='block md:hidden'
+          />
         </div>
       </div>
     </main>
